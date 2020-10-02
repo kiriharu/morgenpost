8cd5d4d9a1e92a7290b962c9b4909add15950d9c
JottyGroups <bard143games@gmail.com>
2020-10-02 20:04:51 +0300

    PEP-8 from config.py import with * replaced by enumerations of constants

--- sender.py
+++ sender.py
@@ -1,19 +1,31 @@
-from config import *
+from config import (
+    STARTING_MESSAGE,
+    WEATHERSTACK_API_KEY, WEATHERSTACK_LOCATIONS,
+    WTTRIN_LOCATIONS,
+    QIWI_TOKEN, QIWI_CROSS_RATES,
+    CBR_CROSS_RATES,
+    BLOCKCHAIN_RATES,
+    COVID_COUNTRIES, COVID_MODE,
+    RSS_FEEDS, RSS_MAX_ENTRIES,
+    TELEGRAM_API_TOKEN, TELEGRAM_USERS_ID
+)
 from api import (
-    weatherstack, qiwi, telegram,
+    weatherstack, qiwi,
     rss, wttr_in, cbr_valutes,
     covid19, blockchain_rates
 )
 from registration import Registration
 
-Registration(STARTING_MESSAGE,
-             (weatherstack.WeatherStack, [WEATHERSTACK_API_KEY, WEATHERSTACK_LOCATIONS]),
-             (wttr_in.WttrIn, [WTTRIN_LOCATIONS]),
-             (qiwi.Qiwi, [QIWI_TOKEN, QIWI_CROSS_RATES]),
-             (cbr_valutes.CbrValutes, [CBR_CROSS_RATES]),
-             (blockchain_rates.BlockchainRates, [BLOCKCHAIN_RATES]),
-             (covid19.Covid19, [COVID_COUNTRIES, COVID_MODE]),
-             (rss.RSS, [RSS_FEEDS, RSS_MAX_ENTRIES]))\
-    .init_telegram(TELEGRAM_API_TOKEN, TELEGRAM_USERS_ID)\
-    .init_apis()\
-    .send()
+
+if __name__ == "__main__":
+    Registration(STARTING_MESSAGE,
+                 (weatherstack.WeatherStack, [WEATHERSTACK_API_KEY, WEATHERSTACK_LOCATIONS]),
+                 (wttr_in.WttrIn, [WTTRIN_LOCATIONS]),
+                 (qiwi.Qiwi, [QIWI_TOKEN, QIWI_CROSS_RATES]),
+                 (cbr_valutes.CbrValutes, [CBR_CROSS_RATES]),
+                 (blockchain_rates.BlockchainRates, [BLOCKCHAIN_RATES]),
+                 (covid19.Covid19, [COVID_COUNTRIES, COVID_MODE]),
+                 (rss.RSS, [RSS_FEEDS, RSS_MAX_ENTRIES])) \
+        .init_telegram(TELEGRAM_API_TOKEN, TELEGRAM_USERS_ID) \
+        .init_apis() \
+        .send()
